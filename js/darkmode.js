5dace773d6d06173b0ce1b8c34aa420e08817581
Yuriko Aya <thechildoflily@gmail.com>
2019-01-17 09:41:34 +0700

    Invisible comment options and using sessionstorage instead of localstorage

--- js/darkmode.js
+++ js/darkmode.js
@@ -6,20 +6,10 @@ function uncheck_the_toggle() {
     document.getElementById("darktoggle").checked = false;
 } 
 
-function rise_from_darkness() {
-    document.getElementsByTagName("BODY")[0].removeAttribute("style", "background-color: #222;");
-    document.getElementById("page").removeAttribute("style", "background-color: black; color: white;");
-    var items = document.getElementsByClassName("menu-item")
-    for (var i = 0; i < items.length; i++) {
-        items[i].removeAttribute("style", "background-color: black; color: white;");
-    }
-    document.getElementById("page").removeAttribute("dark-mode", "true");
-    set_darkness("false");
-}
-
 function fall_into_darkness() {
     document.getElementsByTagName("BODY")[0].setAttribute("style", "background-color: #222;");
     document.getElementById("page").setAttribute("style", "background-color: black; color: white;");
+    document.getElementById("commentform").setAttribute("style", "background-color: #ccc;")
     var items = document.getElementsByClassName("menu-item")
     for (var i = 0; i < items.length; i++) {
         items[i].setAttribute("style", "background-color: black; color: white;");
@@ -29,12 +19,24 @@ function fall_into_darkness() {
 
 }
 
+function rise_from_darkness() {
+    document.getElementsByTagName("BODY")[0].removeAttribute("style", "background-color: #222;");
+    document.getElementById("page").removeAttribute("style", "background-color: black; color: white;");
+    document.getElementById("commentform").removeAttribute("style", "background-color: #ccc;")
+    var items = document.getElementsByClassName("menu-item")
+    for (var i = 0; i < items.length; i++) {
+        items[i].removeAttribute("style", "background-color: black; color: white;");
+    }
+    document.getElementById("page").removeAttribute("dark-mode", "true");
+    set_darkness("false");
+}
+
 function set_darkness(state) {
-    localStorage.setItem("darkness_value",state)
+    sessionStorage.setItem("darkness_value",state)
 }
 
 function check_darkness_status() {
-    var darkness_status = localStorage.getItem("darkness_value");
+    var darkness_status = sessionStorage.getItem("darkness_value");
     if (darkness_status === "true") {
         check_the_toggle();
         fall_into_darkness();
--- simple-darkmode.php
+++ simple-darkmode.php
@@ -11,11 +11,21 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 */
 
 add_action('get_header','add_darkness');
+
 function add_darkness() {
     // add the css
     wp_enqueue_style('simple_darkmode_style', plugins_url( 'css/darkness_style.css', __FILE__ ));
     wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.6.3/css/all.css');
     wp_enqueue_script('simple_darkmode_script', plugins_url( 'js/darkmode.js', __FILE__ ));
+}
+
+add_action('get_footer', 'check_the_darkness',100);
+function check_the_darkness() {
+    ?>
+    <script>
+        check_darkness_status();
+    </script>
+    <?php
 }
 
 add_action('the_content','switches');
@@ -30,10 +40,7 @@ function switches($content) {
         <span class="slider round"></span>
         </label>
         <i class="far fa-moon fa-lg"></i>
-        </center>
-        <script>
-        check_darkness_status();
-        </script>';
+        </center>';
         return $toggle_button.$content;
     } else {
         return $content;
